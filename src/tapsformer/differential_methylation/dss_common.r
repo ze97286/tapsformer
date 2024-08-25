dda67a56fd28bc67ee2f248f53a14bb3244e637f
ze97286 <zohar.etzioni@gmail.com>
2024-08-25 15:59:16 +0100

    fix: dmrcate call

--- src/tapsformer/differential_methylation/dss_common.r
+++ src/tapsformer/differential_methylation/dss_common.r
@@ -21,7 +21,7 @@ bioc_install_and_load <- function(pkg) {
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 bioc_packages <- c(
     "DSS", "GenomicRanges", "bsseq", "org.Hs.eg.db", "bumphunter", "dmrseq",
-    "TxDb.Hsapiens.UCSC.hg38.knownGene", "AnnotationHub", "BiocParallel","DMRcate"
+    "TxDb.Hsapiens.UCSC.hg38.knownGene", "AnnotationHub", "BiocParallel","Gviz", "DMRcate"
 )
 cran_packages <- c("minfi", "Biobase","data.table", "futile.logger", "parallel", "dplyr", "tidyr", "ggplot2", "svglite", "pheatmap", "grid", "gridExtra")
 bioc_install_and_load(bioc_packages)
